5dbf2df60efbe5a79f1db8f3a6102ec5cd6aacca
NguyenMinhHieuu <nguyenminhhieu20010624@gmail.com>
2022-08-08 16:04:08 +0700

    sharethis update

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -167,6 +167,36 @@ const ProductView = (props) => {
 
           {/* <div className="sharethis-inline-share-button">Share</div> */}
 
+          <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+
+              'messenger',
+              'facebook',
+              'twitter'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 25,             // the size of each button (INTEGER)
+
+            // OPTIONAL PARAMETERS
+            url: window.location.href, // (defaults to current url)
+            // image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            // description: 'custom text',       // (defaults to og:description or twitter:description)
+            // title: 'custom title',            // (defaults to og:title or twitter:title)
+            // message: 'custom email text',     // (only for email sharing)
+            // subject: 'custom email subject',  // (only for email sharing)
+            // username: 'custom twitter handle' // (only for twitter sharing)
+          }}
+        />
+
           <div className="product__info__item">
             <div className="product__info__item__title">Màu sắc</div>
             <div className="product__info__item__list">
@@ -246,125 +276,7 @@ const ProductView = (props) => {
           </div>
         </div>
       </div>
-      <div className="product__info">
-        <h1 className="product__info__title">{product.name}</h1>
-        <div className="product__info__item">
-          <span className="product__info__item__price">
-            {/* {numberWithCommas()} */}
-            {product.price}
-          </span>
-        </div>
-
-        {/* <div class="sharethis-inline-share-button">Share</div> */}
-
-        <InlineShareButtons
-          config={{
-            alignment: 'center',  // alignment of buttons (left, center, right)
-            color: 'social',      // set the color of buttons (social, white)
-            enabled: true,        // show/hide buttons (true, false)
-            font_size: 16,        // font size for the buttons
-            labels: 'cta',        // button labels (cta, counts, null)
-            language: 'en',       // which language to use (see LANGUAGES)
-            networks: [           // which networks to include (see SHARING NETWORKS)
-              
-              'messenger',
-              'facebook',
-              'twitter'
-            ],
-            padding: 12,          // padding within buttons (INTEGER)
-            radius: 4,            // the corner radius on each button (INTEGER)
-            show_total: true,
-            size: 25,             // the size of each button (INTEGER)
-
-            // OPTIONAL PARAMETERS
-            url: window.location.href, // (defaults to current url)
-            // image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
-            // description: 'custom text',       // (defaults to og:description or twitter:description)
-            // title: 'custom title',            // (defaults to og:title or twitter:title)
-            // message: 'custom email text',     // (only for email sharing)
-            // subject: 'custom email subject',  // (only for email sharing)
-            // username: 'custom twitter handle' // (only for twitter sharing)
-          }}
-        />
-
-        <div className="product__info__item">
-          <div className="product__info__item__title">Màu sắc</div>
-          <div className="product__info__item__list">
-            {/* {product.colors.map((item, index) => (
-                <div
-                  key={index}
-                  className={`product__info__item__list__item ${
-                    color === item ? "active" : ""
-                  }`}
-                  onClick={() => setColor(item)}
-                >
-                  <div className={`circle bg-${item}`}></div>
-                </div>
-              ))} */}
-          </div>
-        </div>
-        <div className="product__info__item">
-          <div className="product__info__item__title">Kích cỡ</div>
-          <div className="product__info__item__list">
-            {product.sizes?.map((item, index) => (
-              <div
-                key={index}
-                className={`product__info__item__list__item ${
-                  size === item ? "active" : ""
-                }`}
-                onClick={() => setSize(item)}
-              >
-                <span className="product__info__item__list__item__size">
-                  {item}
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="product__info__item">
-          <div className="product__info__item__title">Số lượng</div>
-          <div className="product__info__item__quantity">
-            <div
-              className="product__info__item__quantity__btn"
-              onClick={() => updateQuantity("minus")}
-            >
-              <i className="bx bx-minus"></i>
-            </div>
-            <div className="product__info__item__quantity__input">
-              {quantity}
-            </div>
-            <div
-              className="product__info__item__quantity__btn"
-              onClick={() => updateQuantity("plus")}
-            >
-              <i className="bx bx-plus"></i>
-            </div>
-          </div>
-        </div>
-        <div className="product__info__item">
-          <Button onClick={() => addToCart()}>thêm vào giỏ</Button>
-          <Button onClick={() => goToCart()}>mua ngay</Button>
-        </div>
-      </div>
-      <div
-        className={`product-description mobile ${
-          descriptionExpand ? "expand" : ""
-        }`}
-      >
-        <div className="product-description__title">Chi tiết sản phẩm</div>
-        <div
-          className="product-description__content"
-          dangerouslySetInnerHTML={{ __html: product.description }}
-        ></div>
-        <div className="product-description__toggle">
-          <Button
-            size="sm"
-            onClick={() => setDescriptionExpand(!descriptionExpand)}
-          >
-            {descriptionExpand ? "Thu gọn" : "Xem thêm"}
-          </Button>
-        </div>
-      </div>
+     
     </>
   );
 };
